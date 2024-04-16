745ca0b62a98263402e7419cd5950c4ca92288a7
AITOUALI-Marouane <aitoualimarouane@gmail.com>
2024-04-16 15:15:48 +0200

    Task Model

--- models/task.model.js
+++ models/task.model.js
@@ -1,2 +1,31 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
+
+const TaskSchema = new Schema(
+    {
+        title: {
+            type: String,
+        },
+        description: {
+            type: String,
+        },
+        status: {
+            type: String,
+            enum: ['to-do', 'doing', 'done'],
+            required: false,
+        },
+        dueData: {
+            type: Data
+        },
+        userId: {
+            type: Schema.Types.ObjectId,
+            ref: 'User',
+        }
+    },
+    {
+        timestamps: true,
+    }
+);
+
+const taskSchema = mongoose.model('Task', TaskSchema);
+module.exports = taskSchema;
